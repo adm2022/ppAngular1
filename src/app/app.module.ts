626f1e27f6030fba1b2907a97017aa1712749927
Matt Payne <Payne@MattPayne.org>
2022-01-18 23:55:22 +0000

    Followed first part of https://angular.io/guide/reactive-forms and things are working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 import { StoryEditorComponent } from './story-editor/story-editor.component';
 
 @NgModule({
@@ -12,7 +13,7 @@ import { StoryEditorComponent } from './story-editor/story-editor.component';
     StoryEditorComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
--- src/app/story-editor/story-editor.component.ts
+++ src/app/story-editor/story-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-story-editor',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./story-editor.component.scss']
 })
 export class StoryEditorComponent implements OnInit {
+
+  summary = new FormControl('');
 
   constructor() { }
 
