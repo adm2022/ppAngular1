f744318373547ce574587d1adfdd346ca7e941b8
Matt Payne <Payne@MattPayne.org>
2022-01-18 23:49:59 +0000

    ng generate component StoryEditor

---
+++ src/app/story-editor/story-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-story-editor',
+  templateUrl: './story-editor.component.html',
+  styleUrls: ['./story-editor.component.scss']
+})
+export class StoryEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
