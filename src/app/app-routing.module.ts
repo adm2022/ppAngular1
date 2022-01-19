5c8a5bfdff05450f8b6818482e7df8d80d55cd4b
Matt Payne <Payne@MattPayne.org>
2022-01-19 03:32:07 +0000

    Fixed routing -- noticed it was not broken, just hard to see the about.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from "./about/about.component";
+import {StoryEditorComponent} from "./story-editor/story-editor.component";
 
 
 const routes: Routes = [
+    {
+        path: "",
+        component: StoryEditorComponent
+    },
     {
         path: "about",
         component: AboutComponent
@@ -15,7 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
