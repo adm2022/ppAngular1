4a7adec7b78c630f8a4181e83459aad45605a538
Matt Payne <Payne@MattPayne.org>
2022-01-19 02:09:17 +0000

    Added some material stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatSliderModule } from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { StoryEditorComponent } from './story-editor/story-editor.component';
 
 @NgModule({
@@ -15,6 +26,14 @@ import { StoryEditorComponent } from './story-editor/story-editor.component';
   imports: [
     BrowserModule, ReactiveFormsModule,
     AppRoutingModule,
+    MatSliderModule,MatSidenavModule,MatToolbarModule,MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTabsModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
     BrowserAnimationsModule
   ],
   providers: [],
